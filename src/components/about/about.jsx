cb1bcbbb7d3dc456f41bcc59a68810472c7109c0
Brayan Galvez <brayan.developer20@gmail.com>
2023-11-25 12:01:11 -0500

    Business Analyst

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -23,19 +23,19 @@ const About = () => {
             <article className='about__card'> 
               <FaAward className='about__icon'/>
               <h5>Experience</h5>
-              <small>1+ Year Working</small>
+              <small>2+ Year Working</small>
             </article>
 
             <article className='about__card'> 
               <FiUsers className='about__icon'/>
               <h5>Clients</h5>
-              <small>10+ Worldwide</small>
+              <small>15+ Worldwide</small>
             </article>
 
             <article className='about__card'> 
               <VscFolderLibrary className='about__icon'/>
               <h5>Projects</h5>
-              <small>10+ Completed Projects</small>
+              <small>30+ Completed Projects</small>
             </article>
           </div>
           <p>
